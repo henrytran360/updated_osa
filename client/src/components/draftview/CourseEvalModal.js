ed14cffb4bbccb8f7ba1f25937b166239ea77ff3
QuangNg14 <nnquang.1412@gmail.com>
2022-01-15 13:05:28 -0600

    set up modal evaluation

--- client/src/components/draftview/CourseEvalModal.js
+++ client/src/components/draftview/CourseEvalModal.js
@@ -2,13 +2,6 @@ import React from "react";
 import Modal from "react-modal";
 
 const CourseEvalModal = (props) => {
-    return (
-        <div>
-            <div className="courseCode">
-                {/* {props.subject} {props.courseNum} */}
-                Hello
-            </div>
-        </div>
-    );
+    return <div>hello</div>;
 };
 export default CourseEvalModal;
