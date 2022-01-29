9207c055aa9477f260515788b70ed7d2de609949
ndh4 <ndh4@rice.edu>
2022-01-29 14:00:08 -0600

    Change links in nwb.config.js to dev links

--- client/nwb.config.js
+++ client/nwb.config.js
@@ -3,14 +3,15 @@ module.exports = {
     webpack: {
         define: {
             "process.env.REACT_APP_SERVICE_URL": JSON.stringify(
-                // "http://localhost:3001/auth"
-                "https://develop-dot-ultrascheduler.uc.r.appspot.com/auth"
+                "http://localhost:3001/auth"
+                // "https://develop-dot-ultrascheduler.uc.r.appspot.com/auth"
             ),
             "process.env.REACT_APP_BACKEND_URL": JSON.stringify(
                 "https://api-develop-dot-ultrascheduler.uc.r.appspot.com/api"
             ),
             "process.env.REACT_APP_GRAPHQL_URL": JSON.stringify(
-                "https://api-develop-dot-ultrascheduler.uc.r.appspot.com/graphql"
+                // "https://api-develop-dot-ultrascheduler.uc.r.appspot.com/graphql"
+                "http://localhost:3000/graphql"
             ),
             "process.env.REACT_APP_GRAPHQL_WS_URL": JSON.stringify(
                 "wss://api-develop-dot-ultrascheduler.uc.r.appspot.com/graphql"
