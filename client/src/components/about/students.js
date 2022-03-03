45685c70efb08fe62483ed38c4bf94eeabf8e46f
Jingwen Hu <jinghuw@gmail.com>
2022-03-02 19:26:10 -0600

    yuh new about page (#74)

---
+++ client/src/components/about/students.js
@@ -0,0 +1,139 @@
+import Adrienne_Li from "../../assets/images/Adrienne_Li.jpg";
+import Aidan_Gerber from "../../assets/images/Aidan_Gerber.jpg";
+import Aravind_Kuchibhatla from "../../assets/images/Aravind_Kuchibhatla.jpg";
+import Ariana_Wang from "../../assets/images/Ariana_Wang.jpg";
+import Arthur_Chen from "../../assets/images/Arthur_Chen.jpg";
+import Jamie_Tan from "../../assets/images/Jamie_Tan.jpg";
+import Jing_Hu from "../../assets/images/Jing_Hu.png";
+import Luis_Clague from "../../assets/images/Luis_Clague.jpg";
+import Nathaniel_Hejduk from "../../assets/images/Nathaniel_Hejduk.jpg";
+import Quang_Nguyen from "../../assets/images/Quang_Nguyen.jpg";
+import Theo_Schweizer from "../../assets/images/Theo_Schweizer.jpg";
+import Thushar_Mahesh from "../../assets/images/Thushar_Mahesh.jpg";
+import Yanyu_Zhong from "../../assets/images/Yanyu_Zhong.jpg";
+import Yuka_Aoyama from "../../assets/images/Yuka_Aoyama.jpg";
+export const students = [
+    {
+        team: "2021-2022",
+        people: [
+            { name: "Nathaniel Hejduk", title: "Team Lead", photo: Nathaniel_Hejduk },
+            {
+                name: "Quang Nguyen",
+                title: "Team Lead",
+                photo: Quang_Nguyen,
+            },
+            {
+                name: "Jingwen Hu",
+                title: "Product Manager",
+                photo: Jing_Hu
+            },
+            {
+                name: "Yuka Aoyama",
+                title: "Developer",
+                photo: Yuka_Aoyama,
+            },
+            {
+                name: "Aidan Gerber",
+                title: "Developer",
+                photo: Aidan_Gerber
+            },
+            {
+                name: "Aravind Kuchibhatla",
+                title: "Developer",
+                photo: Aravind_Kuchibhatla
+            },
+            {
+                name: "Theo Schweizer",
+                title: "Developer",
+                photo: Theo_Schweizer
+            },
+            {
+                name: "Ariana Wang",
+                title: "Developer/Designer",
+                photo: Ariana_Wang
+            },
+        ],
+    },
+    {
+        team: "2020-2021",
+        people: [
+            {
+                name: "Will Mundy",
+                title: "Team Lead / Lead Developer",
+                description: "",
+                photo: "",
+                position: 0,
+            },
+            {
+                name: "Cloris Cai",
+                title: "Lead Designer",
+                photo: "",
+            },
+            {
+                name: "Jamie Tan",
+                title: "Team Lead",
+                photo: Jamie_Tan,
+            },
+            {
+                name: "Peter (Donglin) Wang",
+                title: "Developer",
+                photo: "",
+            },
+            {
+                name: "David Torres",
+                title: "Developer",
+                photo: "",
+            },
+            {
+                name: "Manan Bajaj",
+                title: "Developer",
+                photo: "",
+            },
+            {
+                name: "Max Bowman",
+                title: "Developer",
+                photo: "",
+            },
+        ],
+    },
+    {
+        team: "RiceApps OSA 2020",
+        people: [
+            {
+                name: "Arthur Chen",
+                title: "Mentor",
+                photo: Arthur_Chen,
+            },
+            {
+                name: "Luis Clague",
+                title: "Mentor",
+                photo: Luis_Clague,
+            },
+            {
+                name: "Adrienne Li",
+                title: "Contributor",
+                photo: Adrienne_Li,
+            },
+            {
+                name: "Angela Zhang",
+                title: "Contributor",
+                photo: "",
+            },
+            {
+                name: "Yanyu Zhong",
+                title: "Contributor",
+                photo: Yanyu_Zhong,
+            },
+            {
+                name: "Ananya Vaidya",
+                title: "Contributor",
+                photo: "",
+            },
+            {
+                name: "Thushar Mahesh",
+                title: "Contributor",
+                photo: Thushar_Mahesh,
+            },
+        ],
+    },
+];
